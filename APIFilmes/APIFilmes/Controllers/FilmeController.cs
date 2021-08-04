362e6b07d080a7d047f5e3c3b86961d70736bb7a
Torres <dan03torres@gmail.com>
2021-08-04 18:36:41 -0300

    Post Filmes

--- APIFilmes/APIFilmes/Controllers/FilmeController.cs
+++ APIFilmes/APIFilmes/Controllers/FilmeController.cs
@@ -1,11 +1,23 @@
-﻿using System;
+﻿using APIFilmes.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace APIFilmes.Controllers
 {
-    public class FilmeController
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
     {
+        public static List<Filme> filmes = new List<Filme>();
+
+        [HttpPost]
+        public void AdicionarFilme([FromBody] Filme filme)
+        {
+            filmes.Add(filme);
+            Console.WriteLine(filme.Titulo);
+        }
     }
 }
